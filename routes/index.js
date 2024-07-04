be452fe6cfcac19b9f9cb337d4c85a6d35f09854
mariasoto20 <130078854+mariasoto20@users.noreply.github.com>
2024-07-03 23:37:23 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const basedatos = require('../basedatos/models')
 
-/* GET home page. */
-router.get("/dashboard", function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/',(req,res)=>{
+  res.render('index');
 });
-router.get('/', function(req, res, next){
-  res.redirect("/dashboard")
+
+
+router.get('/producto', (req,res)=>{
+  basedatos.getproducto()
+  .then(data =>{
+    console.log(data)
+    res.render('producto', { producto:data});
+  })
+})
+.catch(err => {
+  res.render('producto',{ producto: [] });
+});
+
+router.get('/agrgarproducto', (req,res)=>{
+  res.render('/agrgarproducto')
 })
 
-module.exports = router;
+router.post('/agregarproducto',(req,res)=>{
+const {codigo, nombre,genero,descripcion, precio, category_id} = req.body;
+console.log(codigo, nombre,genero,descripcion, precio, category_id);
+basedatos.insertproducto(codigo, nombre,genero,descripcion, precio, category_id)
+.then(()=>{
+  res.redirect('producto')
+})
+.catch(err =>{
+  console.log(err);
+})
+})
+
