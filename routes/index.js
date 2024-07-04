8e8fd1fe28b5d117b591302b765613800c15231e
mariasoto20 <130078854+mariasoto20@users.noreply.github.com>
2024-07-03 23:49:54 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const basedatos = require('../basedatos/models')
 
-
+router.get("/dashboard", function(req, res, next) {
+  res.render(index');
+});
 router.get('/',(req,res)=>{
   res.render('index');
 });
